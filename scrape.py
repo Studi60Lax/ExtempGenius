1188d26e8d7a78cc5f60392de2f2ab506c28f12e
Script-Hero <neilaylor@hotmail.com>
2020-04-07 21:24:32 -0500

    Wrote get_article_text_from_search_term() function

---
+++ scrape.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+from googlesearch import search
+from newspaper import Article
+
+"""
+Returns array of the text from the top 10 articles that come up given the search term
+"""
+
+def get_article_text_from_search_term(term):
+    articles = []
+    for url in search(term, start=0, stop=10, pause=1):
+        a = Article(url)
+        a.download()
+        a.parse()
+        articles.append(a.text)
+
+    return articles
