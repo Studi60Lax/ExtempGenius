caf8aeef18b34fac48b01e239cdcc79a306fd9c4
Script-Hero <neilaylor@hotmail.com>
2020-04-07 21:51:41 -0500

    Wrote full_clean() function

--- clean_data.py
+++ clean_data.py
@@ -1,9 +1,18 @@
 from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+from nltk import download
 
-def lemmatize(string):
+def full_clean(string):
+    download('stopwords') # Downloads stopwords list if not already on computer
     lemmatizer = WordNetLemmatizer()
-    lemmatized_string = ""
+    stop = set(stopwords.words('english'))
+    cleaned_string = "" # Cleaned text will be built here
+    string = string.lower()
+    # Lemmatize and remove stop words
     for word in string.split(" "):
-        lemmatized_string += lemmatizer.lemmatize(word)
-        lemmatized_string += "
-    return lemmatized_string.strip()
+        if not word in stop: # Removes stopwords
+            cleaned_string += lemmatizer.lemmatize(word) # Lemmatization
+            cleaned_string += " "
+    cleaned_string = cleaned_string.translate ({ord(c): " " for c in "!@#$%^&*()[]{};:,./<>?\|`~-=_+"}) # Replace special chars
+
+    return cleaned_string.strip()
