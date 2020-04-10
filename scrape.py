8653aaa13706634ef7a0b5f39b98d94925c25c58
Script-Hero <neilaylor@hotmail.com>
2020-04-09 22:35:42 -0500

    403/503 error protection in scrape.py

--- scrape.py
+++ scrape.py
@@ -10,9 +10,13 @@ Returns array of the text from the top 10 articles that come up given the search
 def get_articles_text_from_search_term(term):
     articles = []
     for url in search(term, start=0, stop=10, pause=1):
-        a = Article(url)
-        a.download()
-        a.parse()
-        articles.append(a.text)
-
+        try:
+            a = Article(url)
+            a.download()
+            a.parse()
+            articles.append(a.text)
+        except:
+            pass
+            # Prevent stopping all downloads in the event of a 403 or 503 error
+            
     return articles
