83c9025b4d7f1b358a881e628c3166401d8835ca
Script-Hero <neilaylor@hotmail.com>
2020-04-13 23:30:56 -0500

    Removed the part of speech feature engineering

--- Classifier.py
+++ Classifier.py
@@ -8,99 +8,33 @@ from nltk import pos_tag
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.metrics import confusion_matrix, classification_report, accuracy_score
 
+from datetime import datetime
+
+start = datetime.now()
+
 os.environ['TFHUB_CACHE_DIR'] = './tf_cache'
 embed = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
 label_enc = LabelEncoder()
 
+print("Gathering data")
 data = pd.read_csv('sentence_classification_data.csv', header=0)
 data = data.dropna()
 X = data[['Sentence', 'Gram']]
 y = data['Type']
-X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.15,random_state = 1)
+
+label_enc.fit(y)
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2, random_state=1)
+
 
 def process_data(X):
     sentences = X['Sentence'].tolist()
     grams = X['Gram'].tolist()
 
     features = []
-
-    pos_index_map = {
-        'CC' : 0,
-        'CD' : 1,
-        'DT' : 2,
-        'EX' : 3,
-        'FW' : 4,
-        'IN' : 5,
-        'JJ' : 6,
-        'JJR' : 7,
-        'JJS' : 8,
-        'LS' : 9,
-        'MD' : 10,
-        'NN' : 11,
-        'NNS' : 12,
-        'NNP' : 13,
-        'NNPS' : 14,
-        'PDT' : 15,
-        'POS' : 16,
-        'PRP' : 17,
-        'PRP$' : 18,
-        'RB' : 19,
-        'RBR' : 20,
-        'RP' : 21,
-        'SYM' : 22,
-        'TO' : 23,
-        'UH' : 24,
-        'VB' : 25,
-        'VBD' : 26,
-        'VBG' : 27,
-        'VBN' : 28,
-        'VBP' : 29,
-        'VBZ' : 30,
-        'WDT' : 31,
-        'WP' : 32,
-        'WP$' : 33,
-        'WRB' : 34,
-        'RBS' : 35
-    }
-
-    pos_phrase = []
-    pos_sentence = []
-    for i in range(35):
-        pos_phrase.append(0)
-        pos_sentence.append(0)
-
     for i in range(len(sentences)):
         phrase = grams[i]
-
-        phrase_parts_of_speech = pos_tag(phrase)
-        phrase_only_pos = []
-        for p in phrase_parts_of_speech:
-            phrase_only_pos.append(p[1])
-
         sentence = sentences[i]
-        sentence_parts_of_speech = pos_tag(sentence)
-        sentence_only_pos = []
-        for p in sentence_parts_of_speech:
-            sentence_only_pos.append(p[1])
-
-
-
-        for p in phrase_only_pos:
-            try:
-                pos_phrase[pos_index_map[p]] += 1
-            except:
-                pass
-        for p in sentence_only_pos:
-            try:
-                pos_sentence[pos_index_map[p]] += 1
-            except:
-                pass
-
-        phrase_only_pos = ' '.join(phrase_only_pos)
-        sentence_only_pos = ' '.join(sentence_only_pos)
-
-        features.append([phrase, sentence, phrase_only_pos, sentence_only_pos] + pos_phrase + pos_sentence)
-
+        features.append([phrase, sentence])
     return features
 
 def encode_data(X_processed, y=None):
@@ -109,25 +43,25 @@ def encode_data(X_processed, y=None):
     for f in X_processed:
         gram = f[0]
         sentence = f[1]
-        phrase_pos = f[2]
-        sentence_pos = f[3]
-
-        embeddings = embed([gram, sentence, phrase_pos, sentence_pos])
-        new = np.array([embeddings[0], embeddings[1], embeddings[2], embeddings[3]])
+        embeddings = embed([gram, sentence])
+        new = np.array([embeddings[0], embeddings[1]])
         embed_features.append(new.flatten()) # We have to flatten because sklearn only supports 1D features :(
 
     if y.any():
-        y_encoded = label_enc.fit_transform(y)
+        y_encoded = label_enc.transform(y)
 
     return embed_features, y_encoded
 
-clf = GradientBoostingClassifier(n_estimators=100, learning_rate=0.1, random_state=1)
-
+print("Processing.")
 X_train_processed = process_data(X_train)
 X_test_processed = process_data(X_test)
+print("Encoding.")
 X_train_final,y_train_final = encode_data(X_train_processed, y_train)
 X_test_final, y_test_final = encode_data(X_test_processed, y_test)
 
+print("Classifying")
+
+clf = GradientBoostingClassifier(n_estimators=25, learning_rate=0.05, random_state=1)
 clf.fit(X_train_final, y_train_final)
 preds = clf.predict(X_test_final)
 
@@ -140,4 +74,10 @@ print()
 print("Classification Report:")
 print(classification_report(y_test_plaintext, preds_plaintext))
 print("Accuracy:")
-print(accuracy_score(y_test_plaintext, preds_plaintext))
+acc = accuracy_score(y_test_plaintext, preds_plaintext)
+print(acc)
+
+end = datetime.now()
+
+print("RUNTIME")
+print(end-start)
